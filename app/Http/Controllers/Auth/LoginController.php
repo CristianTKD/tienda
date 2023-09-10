087c8d32bd1a09c818e0c8aa2e2d000579995413
Cristian Gutiérrez <cristiangutierreztkd@gmail.com>
2023-09-10 14:36:00 -0400

    CRUD Finalizado con autenticación en Laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -21,6 +22,14 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return $this->loggedOut($request) ?: redirect('/login');
+    }
     /**
      * Where to redirect users after login.
      *
