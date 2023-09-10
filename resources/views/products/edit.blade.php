99d2733defc84bd1c6fc84aeebe000e120b6d0d8
Cristian Gutiérrez <cristiangutierreztkd@gmail.com>
2023-09-10 00:04:04 -0400

    CRUD Productos Laravel

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+@section('contenido')
+    <div class="container"><br>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Editar producto
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('products.update', $product->id) }}" method="POST">
+                            @method('put')
+                            @csrf
+                            <div class="form-group">
+                                <label for="">Descripción</label>
+                                <input type="text" name="description" value="{{ $product->description }}" class="form-control" name="description">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Precio</label>
+                                <input type="number" name="price" value="{{ $product->price }}" class="form-control" name="price">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href="{{ route('products.index') }}" class="btn btn-danger">Cancelar</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
